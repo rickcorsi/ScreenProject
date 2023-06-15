de1afdc448837c559cbb4c47db91c76dc79b3d39
Rick Corsi <cors9056@fredonia.edu>
2023-06-14 21:19:53 -0400

    Add project files.

---
+++ ScreenProject/wwwroot/js/site.js
@@ -0,0 +1,35 @@
+﻿$('#colorbutton-blue').on('click', function () {
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        $('#colorbutton-blue').get(0).requestFullscreen();
+    }
+});
+$('#colorbutton-red').on('click', function () {
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        $('#colorbutton-red').get(0).requestFullscreen();
+    }
+});
+$('#colorbutton-green').on('click', function () {
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        $('#colorbutton-green').get(0).requestFullscreen();
+    }
+});
+$('#colorbutton-white').on('click', function () {
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        $('#colorbutton-white').get(0).requestFullscreen();
+    }
+});
+$('#colorbutton-black').on('click', function () {
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        $('#colorbutton-black').get(0).requestFullscreen();
+    }
+});
